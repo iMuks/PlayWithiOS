3b9c7132db3bd4a07cbecc89274ead1dffaa4f90
mukesh s <mukesh.ct.pce@gmail.com>
2015-07-04 14:04:29 +0530

    First Commit

---
+++ PlayWithiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayWithiOS
+//
+//  Created by Anand Kumar on 7/4/15.
+//  Copyright (c) 2015 Anand Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlayWithiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayWithiOS
+//
+//  Created by Anand Kumar on 7/4/15.
+//  Copyright (c) 2015 Anand Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
